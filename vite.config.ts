d975db58d49e0a197fbb36a62250bc535f9a7945
MOHIT-650 <mohantasarojkumar65@gmail.com>
2026-07-24 22:59:37 +0530

    Configure Cloudflare Workers SvelteKit adapter and wrangler.toml

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -11,13 +11,7 @@ export default defineConfig({
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
 
-			adapter: adapter({
-				pages: 'build',
-				assets: 'build',
-				fallback: '404.html',
-				precompress: false,
-				strict: true
-			})
+			adapter: adapter()
 		})
 	]
 });
